8fac16a97c10f828f9e3cca3c2762bca50800075
Thomas Doom <161301535+Astralion@users.noreply.github.com>
2024-04-23 11:47:35 +0200

    Update login.php

--- php/login.php
+++ php/login.php
@@ -1,56 +1,65 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<link rel="stylesheet" href="../css/style.css">
-	</head>
-
-	<body>
-		<header>
-			<h1>Connexion</h1>
-		</header>
-		<form action="" method="post">
-			<input type="email" id="un" name="email" placeholder="Adresse e-mail" required><br>
-			<input type="password" id="pw" name="password" placeholder="mot de passe" required><br>
-			<label for="c1">
-				<span>Rester connecté</span>
-				<input type="checkbox" id="c1" name="c" value="OK">
-			</label>
-			<button type="submit">Connexion</button>
-			<div class="connexion">
-				<p>Pas encore inscrit ?  <a href="inscription.php">Inscrivez-vous ici.</a></p>
-			</div>
-		</form>
-	</body>
-</html>
-
 <?php
+
+if (!empty($_SERVER['HTTPS'])) {
+    header("Strict-Transport-Security: max-age=31536000");
+}
+
+ini_set('session.cookie_httponly', 1);
+ini_set('session.cookie_secure', 1);
+
 session_start();
 
-try{
-if(isset($_POST['email']) && isset($_POST['password'])){
-    $email = $_POST['email'];
-    $password =$_POST['password'];
+try {
+    if (isset($_POST['email']) && isset($_POST['password'])) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
-    $db = new PDO('mysql:host=localhost;dbname=eiipopolcl55',
-    'is21di91plls',
-    'd6ta5i:7le');
+        $db_host = 'localhost';
+        $db_name = 'eiipopolcl55';
+        $db_user = 'is21di91plls';
+        $db_pass = 'd6ta5i:7le';
 
-    $query = $db->prepare('SELECT * FROM Utilisateur where email_utilisateur = :email');
-    $query->execute(['email' => $email]);
-    $user = $query->fetch(PDO::FETCH_ASSOC);
+  
+        $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
 
-    if($user && password_verify($password, $user['mot_de_passe'])){
-        header('Location: /index.html');
-        echo 'Connexion réussie';
-        exit;
-    } else {
-        echo 'Identifiants incorrects. Veuillez réessayer.';
+        $query = $db->prepare('SELECT * FROM Utilisateur WHERE email_utilisateur = :email');
+        $query->execute(['email' => $email]);
+        $user = $query->fetch(PDO::FETCH_ASSOC);
+
+        if ($user && password_verify($password, $user['mot_de_passe'])) {
+            header('Location: /index.html');
+            exit;
+        } else {
+            echo 'Les informations d\'identification sont incorrectes. Veuillez réessayer.';
+        }
     }
-}
-} catch(PDOException $e) {
+} catch (PDOException $e) {
     echo 'Erreur de base de données : ' . $e->getMessage();
-} catch(Exception $e) {
+} catch (Exception $e) {
     echo 'Une erreur est survenue : ' . $e->getMessage();
 }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <header>
+        <h1>Connexion</h1>
+    </header>
+    <form action="" method="post">
+        <input type="email" id="un" name="email" placeholder="Adresse e-mail" required><br>
+        <input type="password" id="pw" name="password" placeholder="mot de passe" required><br>
+        <label for="c1">
+            <span>Rester connecté</span>
+            <input type="checkbox" id="c1" name="c" value="OK">
+        </label>
+        <button type="submit">Connexion</button>
+        <div class="connexion">
+            <p>Pas encore inscrit ? <a href="inscription.php">Inscrivez-vous ici.</a></p>
+        </div>
+    </form>
+</body>
+</html>
