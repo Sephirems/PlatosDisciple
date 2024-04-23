14917d6ae084fbd8abf8484d5113298f3bf022f4
Thomas Doom <161301535+Astralion@users.noreply.github.com>
2024-04-23 11:57:18 +0200

    Update recherche_oeuvre.php

--- php/recherche_oeuvre.php
+++ php/recherche_oeuvre.php
@@ -1,75 +1,64 @@
+<?php
+if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') {
+    header("Strict-Transport-Security: max-age=31536000");
+}
+ini_set('session.cookie_httponly', 1);
+ini_set('session.cookie_secure', 1);
+session_start();
+?>
 <!DOCTYPE html>
 <meta charset="utf-8">
 <link rel="stylesheet" href="../css/style.css">
 <html>
-	<header>
-		<h1>Bienvenue sur Platos Disciple</h1>
-	</header>
-	<body>
+    <header>
+        <h1>Bienvenue sur Platos Disciple</h1>
+    </header>
+    <body>
         <form action="" method="post">
             <input type="text" name="search" placeholder="Recherche" required>
             <input type="submit" value="Search">
         </form>
-		
-
-
-<?php
-
-if (isset($_POST['search'])) {
-	$search = urlencode($_POST['search']);
-	$url = "https://collectionapi.metmuseum.org/public/collection/v1/search?q=$search";
-	$output = file_get_contents($url);
-	$data = json_decode($output, true);
-
-		if($data['total'] > 0){
-			$objectIDs = array_slice($data['objectIDs'], 0, 20);
-
-			foreach ($objectIDs as $objectID) {
-
-				$url = "https://collectionapi.metmuseum.org/public/collection/v1/objects/$objectID";
-				$output = @file_get_contents($url);
-				$objectData = json_decode($output, true);
-
-			if($output !== FALSE){
-				echo "<h2>" . $objectData['title'] . "</h2>";
-				echo "<p>ObjectID: " . $objectData['objectID'] . "</P>";
-				echo "<p>year: " . $objectData['objectDate'] . "</P>";
-				echo "<p>Culture: " . $objectData['culture'] . "</p>";
-				echo "<p>Artist: " . $objectData['artistDisplayName'] . "</p>";
-				echo "<p>ArtistBio: " . $objectData['artistDisplayBio'] . "</p>";
-				echo "<p>ArtistNationality: " . $objectData['artistNationality'] . "</p>";
-				echo "<p>EndYear: " . $objectData['objectEndDate'] . "</p>";
-				echo "<p>Dimensions: " . $objectData['dimensions'] . "</p>";
-				echo "<p>Country: " . $objectData['country'] . "</P>";
-				echo "<p>Classification: " . $objectData['classification'] . "</p>";
-				
-
-			
-			if ($objectData['isPublicDomain'] && $objectData['primaryImageSmall'] != null) {
-
-				echo"<img src='" . $objectData['primaryImageSmall'] . "' alt='" . $objectData['title'] . "' />";
-
-			} else {
-
-				$restricted_url = "https://collectionapi.metmuseum.org/api/collection/v1/iiif/" . $objectData['objectID'] . "/restricted";
-				$thumbnail_url = "https://collectionapi.metmuseum.org/api/collection/v1/iiif/" . $objectData['objectID'] . "/thumbnail";
-
-			if(!@getimagesize($restricted_url)) {
-
-				echo "<img src='" . $thumbnail_url . "' alt='" . $objectData['title'] . "' />";
-
-				} else {
-
-					echo "<img src='" . $restricted_url . "' alt='" . $objectData['title'] . "'/>";
-				}
-			}
-		}
-	}
-			}else{
-			
-			echo "<p>Pas de résultat</p>";
-			}
-		}
-?>
-	</body>
+        <?php
+        if (isset($_POST['search'])) {
+            $search = urlencode($_POST['search']);
+            $url = "https://collectionapi.metmuseum.org/public/collection/v1/search?q=$search";
+            $output = file_get_contents($url);
+            $data = json_decode($output, true);
+            if($data['total'] > 0){
+                $objectIDs = array_slice($data['objectIDs'], 0, 20);
+                foreach ($objectIDs as $objectID) {
+                    $url = "https://collectionapi.metmuseum.org/public/collection/v1/objects/$objectID";
+                    $output = @file_get_contents($url);
+                    $objectData = json_decode($output, true);
+                    if($output !== FALSE){
+                        echo "<h2>" . $objectData['title'] . "</h2>";
+                        echo "<p>ObjectID: " . $objectData['objectID'] . "</P>";
+                        echo "<p>year: " . $objectData['objectDate'] . "</P>";
+                        echo "<p>Culture: " . $objectData['culture'] . "</p>";
+                        echo "<p>Artist: " . $objectData['artistDisplayName'] . "</p>";
+                        echo "<p>ArtistBio: " . $objectData['artistDisplayBio'] . "</p>";
+                        echo "<p>ArtistNationality: " . $objectData['artistNationality'] . "</p>";
+                        echo "<p>EndYear: " . $objectData['objectEndDate'] . "</p>";
+                        echo "<p>Dimensions: " . $objectData['dimensions'] . "</p>";
+                        echo "<p>Country: " . $objectData['country'] . "</P>";
+                        echo "<p>Classification: " . $objectData['classification'] . "</p>";
+                        if ($objectData['isPublicDomain'] && $objectData['primaryImageSmall'] != null) {
+                            echo"<img src='" . $objectData['primaryImageSmall'] . "' alt='" . $objectData['title'] . "' />";
+                        } else {
+                            $restricted_url = "https://collectionapi.metmuseum.org/api/collection/v1/iiif/" . $objectData['objectID'] . "/restricted";
+                            $thumbnail_url = "https://collectionapi.metmuseum.org/api/collection/v1/iiif/" . $objectData['objectID'] . "/thumbnail";
+                            if(!@getimagesize($restricted_url)) {
+                                echo "<img src='" . $thumbnail_url . "' alt='" . $objectData['title'] . "' />";
+                            } else {
+                                echo "<img src='" . $restricted_url . "' alt='" . $objectData['title'] . "'/>";
+                            }
+                        }
+                    }
+                }
+            } else {
+                echo "<p>Pas de résultat</p>";
+            }
+        }
+        ?>
+    </body>
 </html>
