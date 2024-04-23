e9b5c4a282f52afb9381ce38ad4463084ce97a02
Thomas Doom <161301535+Astralion@users.noreply.github.com>
2024-04-23 10:39:19 +0200

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,14 +1,25 @@
+<?php
+if (!empty($_SERVER['HTTPS'])) {
+    header("Strict-Transport-Security: max-age=31536000");
+}
+ini_set('session.cookie_httponly', 1);
+ini_set('session.cookie_secure', 1);
+session_start();
+?>
 <!DOCTYPE html>
-<meta charset="utf-8">
-<link rel="stylesheet" href="../css/style.css">
 <html>
-	<header>
-		<h1>Bienvenue sur Platos Disciple</h1>
-	</header>
-	<body>
-        <div class="lien">
-            <p><a href="inscription.php">Inscription</a></p>
-            <P><a href="login.php">Connexion</a></P>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Bienvenue sur Platos Disciple</title>
+</head>
+<body>
+    <header>
+        <h1>Bienvenue sur Platos Disciple</h1>
+    </header>
+    <div class="lien">
+        <p><a href="inscription.php">Inscription</a></p>
+        <p><a href="login.php">Connexion</a></p>
     </div>
-	</body>
+</body>
 </html>
