8a4f1901b28367df2be7706893da0b005e15d84d
Thomas Doom <161301535+Astralion@users.noreply.github.com>
2024-04-27 19:45:59 +0200

    Update recherche_oeuvre.php

--- php/recherche_oeuvre.php
+++ php/recherche_oeuvre.php
@@ -5,9 +5,8 @@ if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') {
 ini_set('session.cookie_httponly', 1);
 ini_set('session.cookie_secure', 1);
 session_start();
-$showMoreButton = ''; // Initialisation de la variable $showMoreButton
-$newSearchButton = ''; // Initialisation de la variable $newSearchButton
-?>
+$showMoreButton = ''; 
+$newSearchButton = ''; 
 <!DOCTYPE html>
 <html>
     <head>
@@ -89,7 +88,6 @@ $newSearchButton = ''; // Initialisation de la variable $newSearchButton
         }
         ?>
         </div>
-        <!-- Affichez les boutons ici -->
         <?php echo $showMoreButton; ?>
         <?php echo $newSearchButton; ?>
     </body>
