538a23796c6628787829a99ce82c89627ddb9f1a
Sephirems <166829118+Sephirems@users.noreply.github.com>
2024-05-01 08:31:33 +0200

    Update login.php

--- php/login.php
+++ php/login.php
@@ -13,9 +13,11 @@ try {
         $email = $_POST['email'];
         $password = $_POST['password'];
 
-        $db = new PDO('mysql:host=localhost;dbname=eiipopolcl55',
-        'is21di91plls',
-        'd6ta5i:7le');
+        $db = new PDO(
+            'mysql:host=localhost;dbname=eiipopolcl55',
+            'is21di91plls',
+            'd6ta5i:7le'
+        );
 
         $query = $db->prepare('SELECT * FROM Utilisateur where email_utilisateur = :email');
         $query->execute(['email' => $email]);
@@ -23,6 +25,10 @@ try {
 
         if ($user && password_verify($password, $user['mot_de_passe'])) {
             // Utilisation d'un chemin absolu pour la redirection
+            $_SESSION['loggedUser'] = true;
+            $_SESSION['email'] = $user['email_utilisateur'];
+            $_SESSION['nom_utilisateur'] = $user['nom_utilisateur'];
+            $_SESSION['user_id'] = $user['id_utilisateur'];
             header('Location:index.php');
             exit;
         } else {
@@ -39,15 +45,21 @@ try {
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="../css/style.css">
-        <title>Connexion</title>
-    </head>
-    <body>
-        <header>
-            <h1>Connexion</h1>
-        </header>
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Connexion</title>
+</head>
+
+<body>
+    <header>
+        <h1>Connexion</h1>
+        <a href="index.php">Accueil</a>
+    </header>
+    <?php if (isset($_SESSION['loggedUser'])) : ?>
+        <p>Vous êtes déjà connecter</p>
+    <?php else : ?>
         <form action="" method="post">
             <input type="email" id="un" name="email" placeholder="Adresse e-mail" required><br>
             <input type="password" id="pw" name="password" placeholder="mot de passe" required><br>
@@ -57,9 +69,11 @@ try {
             </label>
             <button type="submit">Connexion</button>
             <div class="connexion">
-                <p>Pas encore inscrit ?  <a href="inscription.php">Inscrivez-vous ici.</a></p>
+                <p>Pas encore inscrit ? <a href="inscription.php">Inscrivez-vous ici.</a></p>
             </div>
         </form>
-        <?php if (isset($message)) echo $message; ?>
-    </body>
+    <?php endif; ?>
+    <?php if (isset($message)) echo $message; ?>
+</body>
+
 </html>
