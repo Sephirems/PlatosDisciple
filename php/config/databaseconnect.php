ef6ba8706f8981374b1f933670b0a8a17759f4b2
Sephirems <166829118+Sephirems@users.noreply.github.com>
2024-05-01 08:43:35 +0200

    Add files via upload

---
+++ php/config/databaseconnect.php
@@ -0,0 +1,12 @@
+<?php
+
+try {
+    $conn = new PDO(
+        sprintf('mysql:host=%s;dbname=%s;port=%s;charset=utf8', MYSQL_HOST, MYSQL_NAME, MYSQL_PORT),
+        MYSQL_USER,
+        MYSQL_PASSWORD
+    );
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('Erreur : ' . $e->getMessage());
+}
