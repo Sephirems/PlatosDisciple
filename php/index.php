3940be9f1c5afef09472be36eae2cd273f89a148
Sephirems <166829118+Sephirems@users.noreply.github.com>
2024-05-01 08:30:44 +0200

    Update index.php

--- php/index.php
+++ php/index.php
@@ -8,18 +8,27 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="../css/style.css">
     <title>Bienvenue sur Platos Disciple</title>
 </head>
+
 <body>
     <header>
         <h1>Bienvenue sur Platos Disciple</h1>
+        <a href="recherche_oeuvre.php">Recherche</a>
+        <?php if (isset($_SESSION['loggedUser'])) : ?>
+            <p><?php echo $_SESSION['nom_utilisateur']; ?></p>
+            <a class="nav-link" href="src/logout.php">Déconnexion</a>
+        <?php else : ?>
+            <div class="lien">
+                <p><a href="inscription.php">Inscription</a></p>
+                <p><a href="login.php">Connexion</a></p>
+            </div>
+        <?php endif; ?>
     </header>
-    <div class="lien">
-        <p><a href="inscription.php">Inscription</a></p>
-        <p><a href="login.php">Connexion</a></p>
-    </div>
 </body>
+
 </html>
