da1bc12b04a498faa549be892fe35dc5cf15c1dc
Sephirems <166829118+Sephirems@users.noreply.github.com>
2024-05-01 08:40:58 +0200

    Update check_like_status.php

--- php/src/check_like_status.php
+++ php/src/check_like_status.php
@@ -1,1 +1,13 @@
+<?php
 
+function check_like_status($conn, $idUtilisateur, $idOeuvre) {
+    $sql = 'SELECT * FROM favoris WHERE id_utilisateur = :idu AND id_oeuvre = :ido';
+    $statement = $conn->prepare($sql);
+    $statement->execute([
+        ':idu' => $idUtilisateur,
+        ':ido' => $idOeuvre
+    ]);
+
+    return $statement->fetch();
+}
+?>
