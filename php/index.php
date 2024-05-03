0a3a5e2b221e100ff623600f0339daa206e4750f
Cloix Remy <remy.cloix@student.hepl.be>
2024-05-03 02:02:06 +0200

    ajout ouevres les plus liké manque une redirection

--- php/index.php
+++ php/index.php
@@ -5,6 +5,12 @@ if (!empty($_SERVER['HTTPS'])) {
 ini_set('session.cookie_httponly', 1);
 ini_set('session.cookie_secure', 1);
 session_start();
+
+require_once(__DIR__ . '/config/mysql.php');
+require_once(__DIR__ . '/config/databaseconnect.php');
+require_once(__DIR__ . '/src/show_most_liked.php');
+require_once(__DIR__ . '/src/check_like_status.php');
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -29,6 +35,54 @@ session_start();
             </div>
         <?php endif; ?>
     </header>
+
+    <?php 
+    $results = show_most_liked($conn);
+    foreach($results as $row) { ?>
+    <div class="result-item">
+                                <h2><?php afficherValeurOuDefaut($row['titre'], 'Titre'); ?></h2>
+                                <?php echo "<img class='search-result-image' src='" . $row['image'] . "' alt='" . $row['titre'] . "' />"; ?>
+                                <p>Culture: <?php afficherValeurOuDefaut($row['culture'], 'Culture'); ?></p>
+                                <p>Artiste: <?php afficherValeurOuDefaut($row['nom_artiste'], 'Artiste'); ?></p>
+                                <p>Bio de l'artiste: <?php afficherValeurOuDefaut($row['bio_artiste'], 'Bio de l\'artiste'); ?></p>
+                                <p>Année de fin: <?php afficherValeurOuDefaut($row['date_fin'], 'Année de fin'); ?></p>
+                                <p>Dimensions: <?php afficherValeurOuDefaut($row['taille'], 'Dimensions'); ?></p>
+                                <p>Pays: <?php afficherValeurOuDefaut($row['pays'], 'Pays'); ?></p>
+                                <p>Classification: <?php afficherValeurOuDefaut($row['classification'], 'Classification'); ?></p>
+
+                                <?php
+                                if (isset($_SESSION['loggedUser'])) {
+                                    $idUtilisateur = $_SESSION['user_id'];
+                                    $idOeuvre = $row['id_oeuvre'];
+                                    $dejalike = check_like_status($conn, $idUtilisateur, $idOeuvre);
+                                    if ($dejalike) {
+                                        echo '<form method="POST" action="src/unlike.php">
+                                            <input type="hidden" name="objectID" value="' . $idOeuvre . '">
+                                            <input type="submit" name="unlike" value="UNLIKE">
+                                        </form>';
+                                    } else {
+                                        echo '<form method="POST" action="src/insertion_oeuvre.php">
+                                        <input type="hidden" name="objectID" value="' . $idOeuvre . '">
+                                        <input type="hidden" name="image" value="' . $row['image'] . '">
+                                        <input type="hidden" name="title" value="' . $row['titre'] . '">
+                                        <input type="hidden" name="culture" value="' . $row['culture'] . '">
+                                        <input type="hidden" name="artistDisplayName" value="' . $row['nom_artiste'] . '">
+                                        <input type="hidden" name="artistDisplayBio" value="' . $row['bio_artiste'] . '">
+                                        <input type="hidden" name="artistNationality" value="' . $row['nationalite_artiste'] . '">
+                                        <input type="hidden" name="objectEndDate" value="' . $row['date_fin'] . '">
+                                        <input type="hidden" name="dimensions" value="' . $row['taille'] . '">
+                                        <input type="hidden" name="country" value="' . $row['pays'] . '">
+                                        <input type="hidden" name="classification" value="' . $row['classification'] . '">
+                                        <input type="submit" value="LIKE">
+                                    </form>';
+                                    }
+                                } else {
+                                     echo "<p>Connectez-vous pour liker cette oeuvre. <a href='login.php'>Se connecter</a></p>";
+                                }
+                            }
+                                ?>
+                            </div>
+
 </body>
 
 </html>
--- php/src/insertion_oeuvre.php
+++ php/src/insertion_oeuvre.php
@@ -1,47 +1,53 @@
-<?php 
+<?php
 
 session_start();
 
 require_once(__DIR__ . '/../config/mysql.php');
 require_once(__DIR__ . '/../config/databaseconnect.php');
 
-$sql = 'INSERT INTO oeuvre(id_oeuvre, image, titre, culture, nom_artiste, bio_artiste, nationalite_artiste, date_fin, taille, pays, classification) VALUES (:id, :im, :ti, :cu, :adn, :adb, :an, :oed, :di, :co, :cl)';
+$sql = 'SELECT COUNT(*) FROM oeuvre WHERE id_oeuvre = :id';
 $statement = $conn->prepare($sql);
+$statement->execute([':id' => htmlspecialchars($_POST['objectID'])]);
+$exists = $statement->fetchColumn() > 0;
 
-try {
-$statement->execute([
-    ':id' => htmlspecialchars($_POST['objectID']),
-    ':im' => htmlspecialchars($_POST['image']),
-    ':ti' => htmlspecialchars($_POST['title']),
-    ':cu' => htmlspecialchars($_POST['culture']),
-    ':adn' => htmlspecialchars($_POST['artistDisplayName']),
-    ':adb' => htmlspecialchars($_POST['artistDisplayBio']),
-    ':an' => htmlspecialchars($_POST['artistNationality']),
-    ':oed' => htmlspecialchars($_POST['objectEndDate']),
-    ':di' => htmlspecialchars($_POST['dimensions']),
-    ':co' => htmlspecialchars($_POST['country']),
-    ':cl' => htmlspecialchars($_POST['classification'])
-]);
-} catch (PDOException $e) {
-    echo 'Erreur lors de l\'insertion des données de l\'oeuvre : ' . $e->getMessage();
+if (!$exists) {
+    $sql = 'INSERT INTO oeuvre(id_oeuvre, image, titre, culture, nom_artiste, bio_artiste, nationalite_artiste, date_fin, taille, pays, classification) VALUES (:id, :im, :ti, :cu, :adn, :adb, :an, :oed, :di, :co, :cl)';
+    $statement = $conn->prepare($sql);
+
+    try {
+        $statement->execute([
+            ':id' => htmlspecialchars($_POST['objectID']),
+            ':im' => htmlspecialchars($_POST['image']),
+            ':ti' => htmlspecialchars($_POST['title']),
+            ':cu' => htmlspecialchars($_POST['culture']),
+            ':adn' => htmlspecialchars($_POST['artistDisplayName']),
+            ':adb' => htmlspecialchars($_POST['artistDisplayBio']),
+            ':an' => htmlspecialchars($_POST['artistNationality']),
+            ':oed' => htmlspecialchars($_POST['objectEndDate']),
+            ':di' => htmlspecialchars($_POST['dimensions']),
+            ':co' => htmlspecialchars($_POST['country']),
+            ':cl' => htmlspecialchars($_POST['classification'])
+        ]);
+    } catch (PDOException $e) {
+        echo 'Erreur lors de l\'insertion des données de l\'oeuvre : ' . $e->getMessage();
+    }
 }
 
 $sql = 'INSERT INTO favoris(id_utilisateur, id_oeuvre) VALUES (:idu, :ido)';
 $statement = $conn->prepare($sql);
 
 try {
-$statement->execute([
-    ':idu' => $_SESSION['user_id'],
-    ':ido' => htmlspecialchars($_POST['objectID'])
-]);
-if(isset($_SESSION['search_url'])) {
-    header('Location: ' . $_SESSION['search_url']);
-    unset($_SESSION['search_url']);
-} else {
-    header('Location: recherche_oeuvre.php');
-}
-exit;
-} catch (PDOException $e){
+    $statement->execute([
+        ':idu' => $_SESSION['user_id'],
+        ':ido' => htmlspecialchars($_POST['objectID'])
+    ]);
+    if (isset($_SESSION['search_url'])) {
+        header('Location: ' . $_SESSION['search_url']);
+        unset($_SESSION['search_url']);
+    } else {
+        header('Location: recherche_oeuvre.php');
+    }
+    exit;
+} catch (PDOException $e) {
     echo 'Erreur lors de l\'insertion des données dans favoris : ' . $e->getMessage();
 }
-?>
